7f167d8e7dddd507934d1488403ee0723db13a91
harshit <harshitsuthar7731@gmail.com>
2021-09-16 13:57:45 +0530

    second commit

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
 app.use(bodyparser({extended:true}));
+app.use(express.static("public"));
 
 app.set('view engine','ejs');
-let item=[];
-//let items;
+let item=["Buy food","Cook food","Eat food"];
+let work=[];
 app.get("/",function(req,res){
     let date = new Date();
     let options ={
@@ -14,13 +15,32 @@ app.get("/",function(req,res){
         month: "long"
     };
     let day = date.toLocaleDateString("en-US",options);
-    res.render("list", {kindofday: day,x: item});
+    res.render("list", {listTitle: day,newListItem: item});
 });
+
+
 app.post("/",function(req,res){
     let items  = req.body.nextItem;
-    item.push(items);
-    res.redirect("/");
+    console.log(req.body);
+    if(req.body.list==="Work")
+    {
+        work.push(items);
+        res.redirect("/work");
+        
+    }
+    else
+    {
+        item.push(items);
+        res.redirect("/");
+    }
  })
+
+ app.get("/work",function(req,res){
+     res.render("list",{listTitle:"Work",newListItem:work})
+ })
+app.get("/about",function(req,res){
+    res.render("about");
+})
 
 
 app.listen(3000,function(){
